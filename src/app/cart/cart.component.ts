ef12b3c499a7dc47fe8c184d74b523ed511a67d7
Richard-Zhang198 <63008315+Richard-Zhang198@users.noreply.github.com>
2022-10-13 11:40:46 -0600

    4 files modified, 8 added

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  constructor(private cartService: CartService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl("/");
+  }
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product, products } from '../product';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -9,7 +10,7 @@ import { Product, products } from '../product';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private cartService: CartService, private route: ActivatedRoute, private router: Router) {
 
    }
 
@@ -17,6 +18,11 @@ export class ProductDetailsComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert(`Your product: ${product.name} has been added to the cart.`);
   }
 
   goBack(): void {
